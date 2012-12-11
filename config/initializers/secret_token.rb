e90846880152aee663ed095e1eecbeb62245e831
vivek <vivek.git@gmail.com>
2012-12-11 12:57:17 +0530

    Intial commit all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DineMedia::Application.config.secret_token = '6d4074908dc50b503e0a856557b3ab317f95b432c086c56177d6ec4b556dd523fe14f73a8b0f0084628d108df751e4e22f51ffef905b23effba9c892d1336dfb'
